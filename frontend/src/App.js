14b7ea70144ff9811e02317c9f8e5e4e3d866a0c
Arjun <yrarjun59@gmail.com>
2023-03-29 16:31:53 +0545

    adding bootstrap and making normal header content

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
+import { Container, Row, Column } from "react-bootstrap"
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
+    <div>
     <Header/>
+    <main className="py-5">
+      <Container>
+        <Row>
+          
+        </Row>
+      </Container>
+    </main>
     <Footer/>
-    </>
+    </div>
   );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
+import {Nav, Form,Button, Container, Navbar} from "react-bootstrap"
+
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">Home 🛍</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/cart"><i className="fas fa-cart"></i>Cart</Nav.Link>
+              <Nav.Link href="#login"><i className="fas fa-user"></i>Login</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+    </Navbar>
+    </div>
   )
 }
 
