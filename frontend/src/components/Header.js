8f6dcac85eebbad7f3aa65c9cc2d943ba234e92d
Arjun <yrarjun59@gmail.com>
2023-04-06 17:37:28 +0545

    adding view for product and rating

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -11,8 +11,12 @@ function Header() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="/cart"><i className="fas fa-cart"></i>Cart</Nav.Link>
-              <Nav.Link href="#login"><i className="fas fa-user"></i>Login</Nav.Link>
+              <Nav.Link href="/cart">
+                <i className="fa-solid fa-cart-shopping"></i>Cart
+                </Nav.Link>
+              <Nav.Link href="/login">
+                <i className="fas fa-user"></i>Login
+                </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
---
+++ frontend/src/components/Products.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import {Link} from "react-router-dom"
+
+
+function Products({product}) {
+  return (
+    <div>
+        <Card className="my-3 p-3 rounded">
+            <Link to={`/product/${product._id}`}>
+                <Card.Img src={product.image}/>
+            </Link>
+        </Card>
+
+        <Card.Body>
+            <Link to={`/product/${product._id}`}>
+                <Card.Title>
+                    <strong>{product.name}</strong>
+                </Card.Title>
+            </Link>
+
+            <Card.Text as="div">
+                <div className="my-3">
+                    {product.rating} from {product.numReviews} reviews
+                    <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#F79028'}/>
+                </div>
+            </Card.Text>
+
+            <Card.Text as="div">
+                <strong>Rs {product.price}</strong>
+            </Card.Text>
+
+        </Card.Body>
+    </div>
+  )
+}
+
+export default Products
