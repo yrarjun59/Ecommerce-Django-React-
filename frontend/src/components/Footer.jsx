3b5bd7d5348e4da94fd10a25096df226ce0a065b
Arjun <yrarjun59@gmail.com>
2023-05-13 14:37:05 +0545

    update footer name

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 function Footer() {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Col className="text-center py-3">Copyright &copy; Ecom</Col>
+                </Row>
+            </Container>
+        </footer>
+    )
 }
 
 export default Footer
