bccab0fdde3b46ace117bad8e18c26eaa2730ec4
Arjun <yrarjun59@gmail.com>
2023-05-23 09:45:27 +0545

    getUserById views and routings

--- backend/base/urls/users_urls.py
+++ backend/base/urls/users_urls.py
@@ -4,11 +4,14 @@ from base.views import users_views as  views
 
 urlpatterns = [
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('register/', views.registerUser, name='register-user'),
+    
     path('profile/', views.getUserProfile, name="user-profile"),
     path('profile/update/', views.updateUserProfile, name="user-profile-update"),
 
     path('', views.getUsers, name="users"),
-    path('register/', views.registerUser, name='register-user'),
-    path('delete/<str:pk>', views.deleteUser, name='delete-user')
+    path('<str:pk>/', views.getUserById, name='user'),
+    path('delete/<str:pk>', views.deleteUser, name='delete-user'),
+    path('update/<str:pk>', views.updateUser, name='update-user'),
 
 ]
